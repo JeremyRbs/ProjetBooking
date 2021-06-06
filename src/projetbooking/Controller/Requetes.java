8656633004c1fbf7e87936a129f3c075dae7804e
Jérémy <71068295+JeremyRbs@users.noreply.github.com>
2021-06-06 15:57:37 +0200

    New_BDD

---
+++ src/projetbooking/Controller/Requetes.java
@@ -0,0 +1,77 @@
+package projetbooking.Controller;
+
+import java.sql.*;
+
+/**
+ *
+ * @author muthk
+ */
+public class Requetes {
+
+    public static Statement connexion() {
+        Connection connection = JDBC_Connect.lot_Connect();
+        Statement statement = null;
+
+        if (connection != null) {
+            try {
+                statement = connection.createStatement();
+            } catch (SQLException ex) {
+                System.out.println("Erreur d'initialisation des requêtes : " + ex);
+            }
+        }
+        return statement;
+    }
+
+    public static void lectureBDD(Statement statement) { //Requetes de lecture
+        //Execution d'une requête SQL simple et récupération du resultat dans un objet ResultSet
+        String chaineSQL = "INSERT INTO `utilisateurs` (`NOM`, `PRENOM`, `MAIL`, `mdp`, `niveau`) VALUES ('MUTH', 'Kévin', 'kevin.muth.auditeur@lecnam.net', '0000', '2') ";
+        ResultSet resultatRequete = null;
+        if (statement != null) {
+            try {
+                resultatRequete = statement.executeQuery(chaineSQL);
+                System.out.println(resultatRequete);
+            } catch (SQLException ex) {
+                System.out.println("Erreur d'exécution de la requête : " + ex);
+            }
+        }
+        if (resultatRequete != null){
+            try{
+                resultatRequete.next();
+            }
+            catch (SQLException ex){
+                
+            }
+        }
+    }
+    
+    public static boolean lot0_1(Statement statement) {
+        
+        String createTable = "INSERT INTO `utilisateurs` (`NOM`, `PRENOM`, `MAIL`, `mdp`, `niveau`) VALUES ('MUTH', 'Kévin', 'kevin.muth.auditeur@lecnam.net', '0000', '2')";
+
+        if (statement != null) {
+            try {
+                return statement.execute(createTable);
+            } catch (SQLException ex) {
+                System.out.println("Erreur de création de la base de donnée " + ex);
+            }
+        }
+        return true;
+    }
+    
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+
+        Statement statement = connexion(); //Opération d'initialisation du driver, de la base de données et du Statement
+        if (statement != null) {
+            System.out.println("Etat initial");
+            lot0_1(statement); //Execution de requête de lecture sur l'objet Statement
+        }
+        Connection connection = statement.getConnection();
+        statement.close();
+        connection.close();
+
+    }
+
+}
