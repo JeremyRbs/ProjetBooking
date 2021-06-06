d0e8921ddd8b608b42e70fb5898c5d55adf5eb11
Jérémy <71068295+JeremyRbs@users.noreply.github.com>
2021-06-06 14:43:47 +0200

    new

--- src/projetbooking/Vue/Fenetre.java
+++ src/projetbooking/Vue/Fenetre.java
@@ -15,27 +15,30 @@ public class Fenetre extends JFrame implements ActionListener {
     ////////////////////////////////////////////////////////////////////////
     
     /////// Bouton quitter pour fermer l'application ///////
-    private JButton bouton_quitter = new JButton("Quitter");
+    private JButton quitButton = new JButton("Quitter");
     
     /////// Panneau courant ///////
-    private JPanel panneauCourant = null;
+    private JPanel currentPanel = null;
     
     /////// Panneau de démarrage ///////
-    private JPanel panneau = new JPanel(new BorderLayout());
-    private JPanel mainPanelPanneau = new JPanel();
-    private JButton bouton_creer_panneau = new JButton("Créer un compte");
-    private JButton bouton_connecter_panneau = new JButton("Connexion");
+    private JPanel startPanel;
+    private JButton b_creation_startPanel;
+    private JButton b_register_startPanel;
     
     /////// Panneau de connexion de compte ///////
-    private JPanel panneauConnexion = new JPanel(new BorderLayout());
-    private JButton bouton_connexion_panneauConnexion = new JButton("Connection");
+    private JPanel fieldPanel_connectionPanel, connectionPanel;
+    private JLabel name_connectionPanel, password_connectionPanel, l_register_connectionPanel;
+    private JTextField nameField_connectionPanel, passwordField_connectionPanel;
+    private JButton b_register_connectionPanel;
     
     /////// Panneau de création de compte ///////
-    private JPanel panneauCreationCompte = new JPanel(new BorderLayout());
-    private JButton bouton_creer_panneauCreationCompte = new JButton("Créer son compte");
+    private JPanel phonePanel_creationAccountPanel, fieldPanel_creationAccountPanel, creationAccountPanel;
+    private JLabel name_creationAccountPanel, email_creationAccountPanel, password_creationAccountPanel, address_creationAccountPanel, mobile_creationAccountPanel, l_register_creationAccountPanel;
+    private JTextField nameField_creationAccountPanel, emailField_creationAccountPanel, passwordField_creationAccountPanel, addressField_creationAccountPanel, mobileField_creationAccountPanel, countryCode_creationAccountPanel;
+    private JButton b_register_creationAccountPanel;
     
     /////// Panneau du menu ///////
-    private JPanel panneauPlanning = new JPanel(new BorderLayout());
+    private JPanel planningButton = new JPanel(new BorderLayout());
 
     ////////////////////////////////////////////////////////////////////////
     ////////////////// Constructeur de la classe Fenetre ///////////////////
@@ -44,47 +47,136 @@ public class Fenetre extends JFrame implements ActionListener {
     public Fenetre()
     {
         
+        //////////////////////////////////////////////////
+        
+        ////////////////////////////////////
+        /////// Panneau de démarrage ///////
+        ////////////////////////////////////
+        
+        //creationAccountPanel et ses composants
+        startPanel = new JPanel();
+        startPanel.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
+        
+        Font font = new Font("MS Sans Serif", Font.BOLD, 18);
+        
+        b_creation_startPanel = new JButton("Se créer un compte");
+        b_creation_startPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b_creation_startPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        
+        b_register_startPanel = new JButton("Se connecter");
+        b_register_startPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b_register_startPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        
+        b_creation_startPanel.addActionListener(this);
+        b_register_startPanel.addActionListener(this);
+        
+        startPanel.add(b_creation_startPanel);
+        startPanel.add(b_register_startPanel);
+        this.setPane(this.startPanel);
+        
+        /////////////////////////////////////////////
+        /////// Panneau de création de compte ///////
+        /////////////////////////////////////////////
+        
+        //phonePanelcreationAccountPanel et ses composants
+        phonePanel_creationAccountPanel = new JPanel();
+        phonePanel_creationAccountPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        mobileField_creationAccountPanel = new JTextField(15);
+        countryCode_creationAccountPanel = new JTextField(2);
+        countryCode_creationAccountPanel.setText("+33");
+        countryCode_creationAccountPanel.setEnabled(false);
+        phonePanel_creationAccountPanel.add(countryCode_creationAccountPanel);
+        phonePanel_creationAccountPanel.add(mobileField_creationAccountPanel);
+
+        //fieldPanel_creationAccountPanel et ses composants
+        fieldPanel_creationAccountPanel = new JPanel();
+        fieldPanel_creationAccountPanel.setLayout(new GridLayout(5,2,3,3));
+        fieldPanel_creationAccountPanel.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
+        name_creationAccountPanel = new JLabel("Nom : ");
+        email_creationAccountPanel = new JLabel("Email : ");
+        password_creationAccountPanel = new JLabel("Mot de passe : ");
+        address_creationAccountPanel = new JLabel("Adresse : ");
+        mobile_creationAccountPanel = new JLabel("Téléphone : ");
+        nameField_creationAccountPanel = new JTextField(15);
+        emailField_creationAccountPanel = new JTextField(15);
+        passwordField_creationAccountPanel = new JTextField(15);
+        addressField_creationAccountPanel = new JTextField(15);
+        fieldPanel_creationAccountPanel.add(name_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(nameField_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(email_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(emailField_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(password_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(passwordField_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(address_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(addressField_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(mobile_creationAccountPanel);
+        fieldPanel_creationAccountPanel.add(phonePanel_creationAccountPanel);
+
+        //creationAccountPanel et ses composants
+        creationAccountPanel = new JPanel();
+        creationAccountPanel.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
+        creationAccountPanel.setLayout(new BoxLayout(creationAccountPanel, BoxLayout.Y_AXIS));
+        
+        font = new Font("MS Sans Serif", Font.BOLD, 18);
+        
+        l_register_creationAccountPanel = new JLabel("Créer un compte");
+        l_register_creationAccountPanel.setFont(font);
+        l_register_creationAccountPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        b_register_creationAccountPanel = new JButton("Créer un compte");
+        b_register_creationAccountPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b_register_creationAccountPanel.addActionListener(this);
+        
+        creationAccountPanel.add(l_register_creationAccountPanel);
+        creationAccountPanel.add(fieldPanel_creationAccountPanel);
+        creationAccountPanel.add(b_register_creationAccountPanel);
+        
+        //////////////////////////////////////////////
+        /////// Panneau de connexion de compte ///////
+        //////////////////////////////////////////////
+        
+        //fieldPanel_connectionPanel et ses composants
+        fieldPanel_connectionPanel = new JPanel();
+        fieldPanel_connectionPanel.setLayout(new GridLayout(5,2,3,3));
+        fieldPanel_connectionPanel.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
+        name_connectionPanel = new JLabel("Nom : ");
+        password_connectionPanel = new JLabel("Mot de passe : ");
+        nameField_connectionPanel = new JTextField(15);
+        passwordField_connectionPanel = new JTextField(15);
+        fieldPanel_connectionPanel.add(name_connectionPanel);
+        fieldPanel_connectionPanel.add(nameField_connectionPanel);
+        fieldPanel_connectionPanel.add(password_connectionPanel);
+        fieldPanel_connectionPanel.add(passwordField_connectionPanel);
+
+        //connectionPanel et ses composants
+        connectionPanel = new JPanel();
+        connectionPanel.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
+        connectionPanel.setLayout(new BoxLayout(connectionPanel, BoxLayout.Y_AXIS));
+        font = new Font("MS Sans Serif", Font.BOLD, 18);
+        l_register_connectionPanel = new JLabel("Se connecter");
+        l_register_connectionPanel.setFont(font);
+        l_register_connectionPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b_register_connectionPanel = new JButton("Connexion");
+        b_register_connectionPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b_register_connectionPanel.addActionListener(this);
+        connectionPanel.add(l_register_connectionPanel);
+        connectionPanel.add(fieldPanel_connectionPanel);
+        connectionPanel.add(b_register_connectionPanel);
+        
+        ///////////////////////////////////
+        /////// Panneau du planning ///////
+        ///////////////////////////////////
+        
+        ////////////////////////////////////////
         /////// Paramètres de la fenêtre ///////
+        ////////////////////////////////////////
+
         this.setTitle("Booking");
         this.setVisible(true);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600,500);
+        this.pack();
         this.centerFrame();
-        ////////////////////////////////////////
-        
-        /////// Panneau de démarrage ///////
-//        this.panneauCourant = this.panneau;
-//        this.setContentPane(this.panneau);
-        this.setPane(this.panneau);
-
-        this.panneau.setLayout(new GridLayout(5,2,3,3));
-        this.panneau.setBorder(BorderFactory.createEmptyBorder(250,250,50,250));
-
-        // this.panneau et ses composants
-        this.panneau.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
-        this.panneau.setLayout(new BoxLayout(this.panneau, BoxLayout.PAGE_AXIS));
-        this.panneau.setBackground(Color.red);
-        this.panneau.add(this.mainPanelPanneau);
-        this.mainPanelPanneau.setBackground(Color.BLUE);
-        
-        /////// Différents boutons ///////
-        
-        // Bouton "Créer un compte"
-        this.mainPanelPanneau.add(this.bouton_creer_panneau);
-        this.bouton_creer_panneau.addActionListener(this);
-        
-        // Bouton "Se connecter"
-        this.mainPanelPanneau.add(this.bouton_connecter_panneau);
-        this.bouton_connecter_panneau.addActionListener(this);
-        
-        // Bouton "Quitter"
-        this.bouton_quitter.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.panneau.add(this.bouton_quitter);
-        this.bouton_quitter.addActionListener(this);
-        
-        this.panneauCreationCompte.add(this.bouton_creer_panneauCreationCompte);
-        
     }
 
     ////////////////////////////////////////////////////////////////////////
@@ -101,35 +193,45 @@ public class Fenetre extends JFrame implements ActionListener {
     
     public void setPane(JPanel pane){
         
-        if(this.panneauCourant != null){
+        if(this.currentPanel != null){
             
-            this.getContentPane().remove(this.panneauCourant);
+            this.getContentPane().remove(this.currentPanel);
             this.repaint();
-            System.out.println("ok");
             
         }
-        
+        System.out.println(pane.getAccessibleContext());
+        System.out.println(pane);
         this.getContentPane().add(pane);
-        pane.validate();
-        this.panneauCourant = pane;
+        this.setContentPane(pane);
+        this.validate();
+        this.currentPanel = pane;
         this.repaint();
+        this.pack();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        System.out.println(e.getActionCommand());
         switch (e.getActionCommand()){
-            
-        }
-        // Si le bouton créer est cliqué
-        if(e.getSource() == this.bouton_creer_panneau){
-            setPane(this.panneauCreationCompte);
-        }
-        
-        // Si le bouton créer est cliqué
-        if(e.getSource() == this.bouton_quitter){
-            System.exit(0);
+            case "Se créer un compte":
+                setPane(this.creationAccountPanel);
+                break;
+            case "Se connecter":
+                setPane(this.connectionPanel);
+                break;
+            case "Créer un compte":
+                // envoi requête
+                // si requête ok
+                // ouverture planning
+                break;
+            case "Connexion":
+                // envoi requête
+                // si requête ok
+                // ouverture planning
+                break;
+            case "Quitter":
+                System.exit(0);
+                break;
         }
     }
 }
